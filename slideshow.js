0dd9a7a20d8566e7e570ebf1de4e87b1fae7602a
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-18 22:58:56 -0500

    created js picture slideshow, added images to correspond with cities and updated worldmap html with script indication

---
+++ slideshow.js
@@ -0,0 +1,14 @@
+var imgIndex = 0;
+imgShow();
+
+function imgShow(){
+  var a;
+  var pics = document.getElementsByClassName("multipic");
+  for (a = 0; a < pics.length; a++) {
+    pics[a].style.display = "none";
+  }
+  imgIndex++;
+  if (pics > pics.length) {imgShow = 1}
+  pics[imgIndex-1].style.display = "block";
+  setTimeout(imgShow, 5000);
+}
