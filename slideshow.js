3b76abbf66172b9c6f34421488d18e9067ac8771
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-19 21:29:33 -0500

    Corrected pics to imgIndex in Javascript and removed multipic array

--- slideshow.js
+++ slideshow.js
@@ -3,12 +3,12 @@ imgShow();
 
 function imgShow(){
   var a;
-  var pics = document.getElementsByClassName('multipic.pic[a]')
+  var pics = document.getElementsByClassName('multipic')
   for (a = 0; a < pics.length; a++) {
     pics[a].style.display = "none";
   }
   imgIndex++;
-  if (pics > pics.length) {imgShow = 1}
+  if (imgIndex > pics.length) {imgIndex = 1}
   pics[imgIndex-1].style.display = "block";
   setTimeout(imgShow, 5000);
 }
