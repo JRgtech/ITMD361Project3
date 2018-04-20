745e720e54c349fc8f679c77c04ea82b113cb918
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-19 21:37:32 -0500

    changed auto change to 2 seconds and changed single qoutes

--- slideshow.js
+++ slideshow.js
@@ -3,12 +3,12 @@ imgShow();
 
 function imgShow(){
   var a;
-  var pics = document.getElementsByClassName('multipic')
+  var pics = document.getElementsByClassName("multipic")
   for (a = 0; a < pics.length; a++) {
     pics[a].style.display = "none";
   }
   imgIndex++;
   if (imgIndex > pics.length) {imgIndex = 1}
   pics[imgIndex-1].style.display = "block";
-  setTimeout(imgShow, 5000);
+  setTimeout(imgShow, 2000);
 }
