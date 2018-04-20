a1191e70b16d51774ca49f3b88eb4ce22d42926c
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-19 21:23:48 -0500

    Changed position from relative to center and added multipic array

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,7 @@ imgShow();
 
 function imgShow(){
   var a;
-  var pics = document.getElementsByClassName('multipic')
+  var pics = document.getElementsByClassName('multipic.pic[a]')
   for (a = 0; a < pics.length; a++) {
     pics[a].style.display = "none";
   }
