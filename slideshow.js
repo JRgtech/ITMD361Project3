9b9e315770f2d5e14a4ace767b42840c56ce9e10
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-22 21:45:46 -0500

    Created an array of images on javascript and removed references in worldmap

--- slideshow.js
+++ slideshow.js
@@ -2,13 +2,34 @@ var imgIndex = 0;
 imgShow();
 
 function imgShow(){
-  var a;
+  var picArray = new Array();
+
+  picArray[0] = new Image();
+  picArray[0].src="https://travel.aarp.org/content/dam/travel/destination-images/france/paris/1400-hero-paris-france-louvre.jpg"
+  picArray[1] = new Image();
+  picArray[1].src="https://media.gettyimages.com/photos/houses-of-parliament-at-night-westminster-london-uk-picture-id508151164"
+  picArray[2] = new Image();
+  picArray[2].scr="https://assets3.thrillist.com/v1/image/2699171/size/gn-gift_guide_variable_c.jpg"
+  picArray[3] = new Image();
+  picArray[3].src="https://www.japantimes.co.jp/wp-content/uploads/2017/10/n-tokyo-a-20171013-870x581.jpg"
+  picArray[4] = new Image();
+  picArray[4].src="https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2e/15/sydney.jpg"
+  picArray[5] = new Image();
+  picArray[5].src="https://i.pinimg.com/originals/7a/7e/8f/7a7e8f73e97a4af01f8fa140a8bdc2ed.jpg"
+  picArray[6] = new Image();
+  picArray[6].src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg"
+  picArray[7] = new Image();
+  picArray[7].src="https://travel.usnews.com/static-travel/images/destinations/44/broadway_getty_fresh_photos_from_all_over_the_world.jpg"
+  picArray[8] = new Image();
+  picArray[8].src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Space_Needle002.jpg/1200px-Space_Needle002.jpg"
+
+  var i;
   var pics = document.getElementsByClassName("multipic")
-  for (a = 0; a < pics.length; a++) {
-    pics[a].style.display = "none";
+  for (i = 0; a < pics.length; i++) {
+    pics[i].appendChild("multipic");
   }
   imgIndex++;
   if (imgIndex > pics.length) {imgIndex = 1}
-  pics[imgIndex-1].style.display = "block";
+  pics[imgIndex-1].appendChild("multipic");
   setTimeout(imgShow, 10000);
 }
