22871aa2d31a32cc371c7162d8c4399ba85db67c
Juan <Juan@Deez-Nutz.hsd1.il.comcast.net>
2018-04-23 21:46:45 -0500

    Simplified array declaration and modified array loop, added second image placeholder

--- slideshow.js
+++ slideshow.js
@@ -1,31 +1,31 @@
 imgShow();
 function imgShow(){
-  var picArray = new Array();
 
-  picArray[0] = new Image();
-  picArray[0].src="https://travel.aarp.org/content/dam/travel/destination-images/france/paris/1400-hero-paris-france-louvre.jpg"
-  picArray[1] = new Image();
-  picArray[1].src="https://media.gettyimages.com/photos/houses-of-parliament-at-night-westminster-london-uk-picture-id508151164"
-  picArray[2] = new Image();
-  picArray[2].scr="https://assets3.thrillist.com/v1/image/2699171/size/gn-gift_guide_variable_c.jpg"
-  picArray[3] = new Image();
-  picArray[3].src="https://www.japantimes.co.jp/wp-content/uploads/2017/10/n-tokyo-a-20171013-870x581.jpg"
-  picArray[4] = new Image();
-  picArray[4].src="https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2e/15/sydney.jpg"
-  picArray[5] = new Image();
-  picArray[5].src="https://i.pinimg.com/originals/7a/7e/8f/7a7e8f73e97a4af01f8fa140a8bdc2ed.jpg"
-  picArray[6] = new Image();
-  picArray[6].src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg"
-  picArray[7] = new Image();
-  picArray[7].src="https://travel.usnews.com/static-travel/images/destinations/44/broadway_getty_fresh_photos_from_all_over_the_world.jpg"
-  picArray[8] = new Image();
-  picArray[8].src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Space_Needle002.jpg/1200px-Space_Needle002.jpg"
+var picArray ["https://travel.aarp.org/content/dam/travel/destination-images/france/paris/1400-hero-paris-france-louvre.jpg",
 
-  var i;
-  var oldpic = document.getElementsByClassName("multipic");
-  for (i = 0; a < picArray.length; i++) {
-    oldpic.appendChild(picArray[i]);
-  }
-  if (i > picArray.length) {i = 0};
-  setTimeout(imgShow, 2000);
+"https://media.gettyimages.com/photos/houses-of-parliament-at-night-westminster-london-uk-picture-id508151164",
+
+"https://assets3.thrillist.com/v1/image/2699171/size/gn-gift_guide_variable_c.jpg",
+
+"https://www.japantimes.co.jp/wp-content/uploads/2017/10/n-tokyo-a-20171013-870x581.jpg",
+
+"https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2e/15/sydney.jpg",
+
+"https://i.pinimg.com/originals/7a/7e/8f/7a7e8f73e97a4af01f8fa140a8bdc2ed.jpg",
+
+"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg",
+
+"https://travel.usnews.com/static-travel/images/destinations/44/broadway_getty_fresh_photos_from_all_over_the_world.jpg",
+
+"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Space_Needle002.jpg/1200px-Space_Needle002.jpg"];
+
+var i;
+var oldpic = document.getElementsByClassName("placeHolder");
+for (i = 0; a < picArray.length; i++) {
+  var tempImg = document.createElement('img');
+  tempImg.src = picArray[i];
+  oldpic.appendChild(tempImg);
+}
+if (i > picArray.length) {i = 0};
+setTimeout(imgShow, 2000);
 }
