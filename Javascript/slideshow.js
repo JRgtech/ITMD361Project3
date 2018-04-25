44811224c1f2b81ccdde1b0673d524d26ce26b91
Juan <Juan@dhcp64-131-120-114.kentlaw.edu>
2018-04-25 15:44:41 -0500

    Moved img links to travels page and added id and onclick events to button. Modified css to not display img links and modified javascript function

--- Javascript/slideshow.js
+++ Javascript/slideshow.js
@@ -1,49 +1,5 @@
 imgShow();
 
 function imgShow()  {
-
-
-var imglinks ["https://travel.aarp.org/content/dam/travel/destination-images/france/paris/1400-hero-paris-france-louvre.jpg",
-
-"https://media.gettyimages.com/photos/houses-of-parliament-at-night-westminster-london-uk-picture-id508151164",
-
-"https://assets3.thrillist.com/v1/image/2699171/size/gn-gift_guide_variable_c.jpg",
-
-"https://www.japantimes.co.jp/wp-content/uploads/2017/10/n-tokyo-a-20171013-870x581.jpg",
-
-"https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2e/15/sydney.jpg",
-
-"https://i.pinimg.com/originals/7a/7e/8f/7a7e8f73e97a4af01f8fa140a8bdc2ed.jpg",
-
-"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg",
-
-"https://travel.usnews.com/static-travel/images/destinations/44/broadway_getty_fresh_photos_from_all_over_the_world.jpg",
-
-"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Space_Needle002.jpg/1200px-Space_Needle002.jpg"]
-
-var newimg;
-
-var a = 0;
-var b = 0;
-
-for (a > imglinks.length; a++)
-{
-  newimg.src = imglinks[a]
-}
-
-if (a < imglinks.length) {
-  a = 0;
-}
-
-for (b > pressbutton.length; b++) {
-  if (pressbutton(b).clicked == true)
-  {
-    var originalImg = document.getElementsById("b");
-    newimg.appendChild(originalImg);
-  }
-
-if (b < pressbutton.length) {
-  b = 0;
-  newimg = "none"
-}
+document.getElementByClass("imghold").src = imgContain(#im_);
 }
