ac86b49fc053971d77d95bd1f8ab0f93655513a6
Juan <Juan@dhcp64-131-120-114.kentlaw.edu>
2018-04-25 17:57:56 -0500

    added getElement to onclick event with innerHTML, corrected css title for styling of imgs and changed javascript

--- Javascript/slideshow.js
+++ Javascript/slideshow.js
@@ -1,7 +1,6 @@
 function imgShow() {
 
-var tempclass = document.getElementByClass("imgContain").value;
-var tempID = document.getElementById(imgContain.id).value
+var tempclass = document.getElementByClass("imgContain")
 var oldImg = document.getElementByClass("imghold");
 oldImg.appendChild(tempID);
 }
